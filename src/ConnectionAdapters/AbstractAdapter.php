ea1af4c24416f13ca994b01367cad41b326f5fb2
Andrius Petrauskis <2466985+andriuspetrauskis@users.noreply.github.com>
2018-01-20 20:18:40 +0200

    Implement some functionality in Abstract connection service

---
+++ src/ConnectionAdapters/AbstractAdapter.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace MicroSymfony\Connection\ConnectionAdapters;
+
+use MicroSymfony\Connection\DiscoveryAdapters\DiscoveryAdapterInterface;
+
+abstract class AbstractAdapter implements ConnectionAdapterInterface
+{
+    /** @var string[] */
+    protected $serviceIps = [];
+    /** @var DiscoveryAdapterInterface */
+    protected $discovery;
+    /** @var string */
+    protected $serviceHeader;
+    /** @var array */
+    protected $additionalHeaders = [];
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function requestRaw(string $method, string $uri, array $params = []): string;
+
+    /**
+     * @inheritdoc
+     */
+    public function request(string $method, string $endpoint, array $params = []): string
+    {
+        $serviceInfo = explode('/', $endpoint, 2);
+        $serviceName = $serviceInfo[0];
+        $path = $serviceInfo[1] ?? '';
+
+        $serviceIp = $this->getServiceIp($serviceName);
+
+        $uri = $serviceIp.'/'.$path;
+
+        $params['headers'] = $this->mergeHeaders($serviceName, $params['headers'] ?? []);
+
+        return $this->requestRaw($method, $uri, $params);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function get(string $endpoint, array $params = []): string
+    {
+        return $this->request('GET', $endpoint, $params);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function post(string $endpoint, array $params = []): string
+    {
+        return $this->request('POST', $endpoint, $params);
+    }
+
+    /**
+     * Get service IP from service discovery
+     *
+     * @param string $serviceName
+     * @param bool   $force
+     * @return string
+     */
+    protected function getServiceIp(string $serviceName, bool $force = false): string
+    {
+        if (!isset($this->serviceIps) || $force) {
+            $serviceIp = $this->discovery->discover($serviceName);
+            $this->serviceIps[$serviceName] = $serviceIp;
+        }
+
+        return $this->serviceIps[$serviceName];
+    }
+
+    protected function mergeHeaders(string $serviceName, array $headers = []): array
+    {
+        if (!empty($this->serviceHeader)) {
+            $headers = array_merge($headers, [$this->serviceHeader => $serviceName]);
+        }
+        if (!empty($this->additionalHeaders)) {
+            $headers = array_merge($headers, $this->additionalHeaders);
+        }
+
+        return $headers;
+    }
+
+    /**
+     * @param array $additionalHeaders
+     */
+    public function setAdditionalHeaders(array $additionalHeaders): void
+    {
+        $this->additionalHeaders = $additionalHeaders;
+    }
+
+    /**
+     * @param string $header
+     * @param string|int $value
+     */
+    public function addAdditionalHeader(string $header, $value): void
+    {
+        $this->additionalHeaders[$header] = $value;
+    }
+
+    /**
+     * @param string $serviceHeader
+     */
+    public function setServiceHeader(string $serviceHeader): void
+    {
+        $this->serviceHeader = $serviceHeader;
+    }
+}
